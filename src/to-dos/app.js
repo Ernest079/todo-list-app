df0c482b9b875381d20a2bf5330dee31ee06b9c0
Ernest079 <ernestomendoza.martinez73@gmail.com>
2025-02-04 13:56:07 -0600

    Add event listeners for adding, toggling, and deleting todos

--- src/to-dos/app.js
+++ src/to-dos/app.js
@@ -4,6 +4,7 @@ import { renderTdos } from './use-cases';
 
 const ElementIDs = {
   TodoList: '.todo-list',
+  NewTodoImput: '#new-todo-input',
 }
 /**
  * 
@@ -23,4 +24,36 @@ export const App = (elementId) => {
     document.querySelector(elementId).append(app);
     displayTodos();
   })();
+
+  //Referencias HTML
+  const newDesciptionInput = document.querySelector(ElementIDs.NewTodoImput);
+  const todoListUL = document.querySelector(ElementIDs.TodoList);
+  const destroyTodo = document.querySelector(ElementIDs.TodoList);
+
+  //events
+  newDesciptionInput.addEventListener('keyup', (event) => {
+    if(event.keyCode !== 13){
+      return;
+    }
+    if(event.target.value.trim().length === 0){
+      return;
+    }
+    todoStore.addTodo(event.target.value);
+    displayTodos();
+    event.target.value = '';
+  });
+
+  todoListUL.addEventListener('click', (event) => {
+    const element = event.target.closest('[data-id]');
+    todoStore.toggleTodo(element.getAttribute('data-id'));
+    displayTodos();
+  });
+
+  destroyTodo.addEventListener('click', (event) => {
+    const element = event.target.closest('[data-id]');
+    if(event.target.className==="destroy"){
+      todoStore.deleteTodo(element.getAttribute('data-id'));
+    }
+    displayTodos();
+  });
 }
